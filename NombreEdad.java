ad6fe6c4d88f13fda35d602361aa66f7aad2fb6d
Denis-Micu <ionut.micu.al@iespoligonosur.org>
2025-09-30 16:44:13 +0000

    Ejercicio2

---
+++ NombreEdad.java
@@ -0,0 +1,8 @@
+package  workspaces;
+public class NombreEdad {
+    public static void main(String[] args) {
+    String name = "Ionut Denis Micu ";
+    int edad = 17;
+    System.out.println("Me llamo " + name + " " + "tengo "+ edad);
+    }
+}
