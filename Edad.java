3d665a4bc7796672fa3eb21b09ad8bd1a5b23d32
Denis-Micu <ionut.micu.al@iespoligonosur.org>
2025-09-30 16:54:39 +0000

    Ejercicio3

---
+++ Edad.java
@@ -0,0 +1,10 @@
+package  workspaces;
+import java.util.Scanner;
+public class Edad {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Escribe tu edad");
+    int edad = sc.nextInt();
+    System.out.println("Tienes " + edad + " años");
+    }
+}
