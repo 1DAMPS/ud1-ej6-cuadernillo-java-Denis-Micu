9125ac2ca2a0486d45213a3fd13b43406afbfffd
Denis-Micu <ionut.micu.al@iespoligonosur.org>
2025-10-01 17:16:09 +0000

    Ejercicio 6

---
+++ Ej6.java
@@ -0,0 +1,8 @@
+public class Ej6{
+    public static void main(String[]args){
+        String nombreProfesor = "Pepe";
+        String estadoCivil = "Divorciado";
+        String direccionProfesor = "Calle calle 2";
+        System.out.println(nombreProfesor  + "\n" + estadoCivil +"\n"+ direccionProfesor);
+    }
+}
