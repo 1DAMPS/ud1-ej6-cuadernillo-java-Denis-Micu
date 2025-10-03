089e72794b87e47bef0a28f4ba147bc8a1f8981a
Denis-Micu <ionut.micu.al@iespoligonosur.org>
2025-10-03 14:11:52 +0000

    Ej9.java

---
+++ Ej9.java
@@ -0,0 +1,12 @@
+//Declara una variable int sin inicializar y observa qué ocurre si intentas usarla en un System.out.println. Escríbelo en un comentario dentro del código.
+public class Ej9{
+    public static void main(String[]args){
+    int numero1;
+    System.out.println(numero1);
+
+
+
+
+        }
+    }
+    //Da un error que dice que la variable no esta inicializada.
