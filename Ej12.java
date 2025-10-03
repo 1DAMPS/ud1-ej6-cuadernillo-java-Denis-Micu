c7ddcd7e6ba7eeddfe1eb588f66f09d352960e20
Denis-Micu <ionut.micu.al@iespoligonosur.org>
2025-10-03 14:23:20 +0000

    Ej12.java

---
+++ Ej12.java
@@ -0,0 +1,8 @@
+public class Ej12 {
+    public static void main(String[] args) {
+        float pi = 3.14; 
+        System.out.println(pi);
+    }
+}
+//error: incompatible types: possible lossy conversion from double to float
+//       float pi = 3.14; 
