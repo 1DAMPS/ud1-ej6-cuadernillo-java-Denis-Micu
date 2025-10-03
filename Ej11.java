d7b6472bfbde27e2b244c82faf4487139b8be63a
Denis-Micu <ionut.micu.al@iespoligonosur.org>
2025-10-03 14:18:19 +0000

    Ej11

---
+++ Ej11.java
@@ -0,0 +1,10 @@
+
+public class Ej11 {
+    public static void main(String[] args) {
+        
+        long poblacionMundial = 8000000000L; 
+
+      
+        System.out.println(poblacionMundial);
+    }
+}
