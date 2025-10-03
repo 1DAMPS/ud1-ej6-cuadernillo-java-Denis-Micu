7ac2b77f548c271762f1f89caee61c1e9c1fc11f
Denis-Micu <ionut.micu.al@iespoligonosur.org>
2025-10-03 14:54:22 +0000

    Ej14.java

---
+++ Ej14.java
@@ -0,0 +1,9 @@
+//Declara una constante con el número de días de la semana y úsala en un programa que lo muestre por pantalla: “La semana tiene X días”.
+public class Ej14 {
+    public static void main(String[] args) {
+        final int DIAS_SEMANA = 7;
+
+      
+        System.out.println("La semana tiene " + DIAS_SEMANA + " días");
+    }
+}
