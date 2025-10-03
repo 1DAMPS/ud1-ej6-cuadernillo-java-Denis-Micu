4ded2f9ad9f845dfcd34a7b61926097d3ac13965
Denis-Micu <ionut.micu.al@iespoligonosur.org>
2025-10-03 14:09:07 +0000

    Ej8

---
+++ Ej8.java
@@ -0,0 +1,15 @@
+// Declara e inicializa variables de tipo int, double, char y boolean, y muestra su valor por pantalla.
+public class Ej8{
+    public static void main(String[]args){
+    int numero1 = 1;
+    double numeroDecimal = 2.1;
+    char caracter = 'a';
+    boolean booleano = true;
+    System.out.println(numero1);
+    System.out.println(numeroDecimal);
+    System.out.println(caracter);
+    System.out.println(booleano);
+ 
+ 
+    }
+}
