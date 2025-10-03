ffd9a36ee82a260159e698143b2e881df6e9f798
Denis-Micu <ionut.micu.al@iespoligonosur.org>
2025-10-03 14:16:00 +0000

    Ej10.java

---
+++ Ej10.java
@@ -0,0 +1,10 @@
+//Declara variables de tipo byte y short. Asígnales valores dentro de su rango y muestra el resultado.
+public class Ej10{
+    public static void main(String[]args){
+    byte numero1 = 1;
+    short numero2 = 100;
+    System.out.println(numero1);
+    System.out.println(numero2);
+
+    }
+}
