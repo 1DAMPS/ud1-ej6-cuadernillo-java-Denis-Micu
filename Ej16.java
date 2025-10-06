9b93f91edd0ed42561cf7e985488d4444fd58af8
Denis-Micu <ionut.micu.al@iespoligonosur.org>
2025-10-06 20:03:44 +0200

    Ej16

---
+++ Ej16.java
@@ -0,0 +1,10 @@
+//Declara una variable de tipo short con valor 30 000. Multiplícala por 
+//10 y muestra el resultado. ¿Qué observas? Explica por qué ocurre en un comentario dentro del código.
+
+public class Ej16 {
+    public static void main(String[] args) {
+        short numero = 30000;
+        numero = (short)(numero * 10);
+        System.out.println(numero);
+    }
+}
