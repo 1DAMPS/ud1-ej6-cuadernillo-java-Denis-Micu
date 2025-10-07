7b76fc1116c6fbf2f04a5eb8ce1658fd8f842bd6
Denis-Micu <ionut.micu.al@iespoligonosur.org>
2025-10-07 18:39:21 +0200

    Ej18.java

---
+++ Ej18.java
@@ -0,0 +1,15 @@
+//Escribe un programa con el contenido que quieras, con al menos tres comentarios: uno de línea, uno multilínea y uno al
+//inicio con tu nombre, la fecha y el propósito del programa (de cabecera).
+
+/* @author  Denis Micu
+ * @since   07-10-2025
+ * El proposito es dar la bienvenida al programa */
+
+
+public class Ej18 {
+    public static void main(String[] args) {
+        // Imprimir un saludo
+        System.out.println("Hola, bienvenid@ al programa :)");
+
+    }
+}
