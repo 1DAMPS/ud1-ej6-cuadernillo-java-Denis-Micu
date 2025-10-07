317fb68277e6f2d495ee6d9d7a4b5f0a09feaa24
Denis-Micu <ionut.micu.al@iespoligonosur.org>
2025-10-07 18:30:44 +0200

    Ej18

---
+++ Ej18.java
@@ -0,0 +1,3 @@
+int radio = 5;//Se declara y se inicializa la variable radio que es un int.
+double area = 3.1416 * radio * radio;//se declara area de tipo double y se inicializa con el valor de pi por el rado al cuadrado
+System.out.println(area);//se imprime por pantalla el valor de area
